f14011802afabd0243f7ab7d79df3c0f5c92d9a8
Emmanuel <epodesta158@gmail.com>
2018-05-23 22:54:56 -0300

    Removed some files.

--- anexos/fur/furWithoutPskel/run.sh
+++
@@ -1,3 +0,0 @@
-MPPADIR=/usr/local/k1tools
-
-$MPPADIR/bin/k1-jtag-runner --multibinary=output/bin/Fur_noAbstraction.img --exec-multibin=IODDR0:master
--- anexos/gol/run.sh
+++
@@ -1,3 +0,0 @@
-MPPADIR=/usr/local/k1tools
-
-$MPPADIR/bin/k1-jtag-runner --multibinary=output/bin/pskel.img --exec-multibin=IODDR0:master -- $1 $2 $3 $4 $5 $6 $7
